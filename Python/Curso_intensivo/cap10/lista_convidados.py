da5d366d127a165391d2195d6beea6fd5cc8dcb0
Deyved Antonio <deyved.antonio@gmail.com>
2023-02-28 22:32:01 -0300

    Exercício 10.4

---
+++ Python/Curso_intensivo/cap10/lista_convidados.py
@@ -0,0 +1,5 @@
+while True:
+    nome_usuario = input('Qual é o nome do usuário: ')
+    print(f'Seja bem vindo {nome_usuario}!')
+    with open('guest_book.txt', 'a') as file_object:
+        file_object.write(f'visita do usuario {nome_usuario}.\n')
